d15e59e703a13e1e44e6247a9f544f29b11ac36c
huypn <huypn@Desktop088.nttdata.com.vn>
2017-11-13 17:26:48 +0700

    thanh nhan queenly

---
+++ queenly/resources/views/layouts/navigator.blade.php
@@ -0,0 +1,31 @@
+<div id="page-container">
+        <header class="">
+            <div class="container">
+                <!-- Site Logo -->
+                <a href="https://corsetacademy.net/" class="site-logo">
+				<img src="./How to sew wedding dresses, corset, evening gowns. Sewing patterns Corset Academy_files/logo.png" alt="Corset Academy">
+			</a>
+                <!-- Site Logo -->
+
+                <!-- Site Navigation -->
+                <nav>
+                    <!-- Main Menu -->
+                    <ul class="site-nav">
+
+                        <li class="hidden">
+											<a data-toggle="modal" href="https://corsetacademy.net/categories/#feedback">LEAVE FEEDBACK</a>
+										</li><li class="">
+											<a href="https://corsetacademy.net/price/">PRICING</a>
+										</li><li class="">
+											<a href="https://corsetacademy.net/corset-pattern-alterations-calculation/">Pattern Alteration</a>
+										</li><li class="">
+											<a href="https://corsetacademy.net/category/blog/success-stories/">Success Stories</a>
+										</li><li class="">
+											<a href="https://corset-pattern.com/">Shop</a>
+										</li>                    </ul>
+                    <!-- END Main Menu -->
+                </nav>
+                <!-- END Site Navigation -->
+            </div>
+        </header>
+    </div>
