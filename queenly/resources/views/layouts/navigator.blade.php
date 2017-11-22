aec73d21dddc9480f0094f0106b721b1c24f5436
huypn <huypn@Desktop088.nttdata.com.vn>
2017-11-22 09:45:17 +0700

    logo update

--- queenly/resources/views/layouts/navigator.blade.php
+++ queenly/resources/views/layouts/navigator.blade.php
@@ -1,31 +1,28 @@
 <div id="page-container">
-        <header class="">
-            <div class="container">
-                <!-- Site Logo -->
-                <a href="/" class="site-logo">
-				<img src="{{ asset('images/logo.png') }}" alt="Corset Academy">
-			</a>
-                <!-- Site Logo -->
+	<header class="">
+		<div class="container">
+			<!-- Site Logo -->
+			<a href="/" class="site-logo"> </a>
+			<!-- Site Logo -->
 
-                <!-- Site Navigation -->
-                <nav>
-                    <!-- Main Menu -->
-                    <ul class="site-nav">
-
-                        <li class="hidden">
-											<a data-toggle="modal" href="#categories/#feedback">LEAVE FEEDBACK</a>
-										</li><li class="">
-											<a href="#/price/">PRICING</a>
-										</li><li class="">
-											<a href="#/corset-pattern-alterations-calculation/">Pattern Alteration</a>
-										</li><li class="">
-											<a href="#/category/blog/success-stories/">Success Stories</a>
-										</li><li class="">
-											<a href="#/">Shop</a>
-										</li>                    </ul>
-                    <!-- END Main Menu -->
-                </nav>
-                <!-- END Site Navigation -->
-            </div>
-        </header>
-    </div>
+			<!-- Site Navigation -->
+			<nav>
+				<!-- Main Menu -->
+				<ul class="site-nav">
+					<li class=""><a data-toggle="modal"
+						href="/">HOME</a></li>
+					<li class="hidden"><a data-toggle="modal"
+						href="#categories/#feedback">LEAVE FEEDBACK</a></li>
+					<li class=""><a href="#/price/">PRICING</a></li>
+					<li class=""><a href="#/corset-pattern-alterations-calculation/">Pattern
+							Alteration</a></li>
+					<li class=""><a href="#/category/blog/success-stories/">Success
+							Stories</a></li>
+					<li class=""><a href="#/">Shop</a></li>
+				</ul>
+				<!-- END Main Menu -->
+			</nav>
+			<!-- END Site Navigation -->
+		</div>
+	</header>
+</div>
