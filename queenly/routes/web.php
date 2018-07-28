f70d4ad685952c53d320b63e7bd1923d7e687cb0
HuyPN <HuyPN@192.168.1.68>
2018-07-28 21:51:46 +0700

    add

--- queenly/routes/web.php
+++ queenly/routes/web.php
@@ -67,7 +67,6 @@ Route::get('/weddingeveningdressestheoretical/tut{id}', 'PostController@weddinge
 Route::get('/weddingeveningdressespractical/tut{id}', 'PostController@weddingeveningdressespractical');
 Route::get('/weddingskirtspetticoatstheoretical/tut{id}', 'PostController@weddingskirtspetticoatstheoretical');
 Route::get('/weddingskirtspetticoatspractical/tut{id}', 'PostController@weddingskirtspetticoatspractical');
-
 Route::get('/structureddesignerskirt/tut{id}', 'PostController@structureddesignerskirt');
 Route::get('/fancydresstwentyyearsoldgirl/tut{id}', 'PostController@fancydress12yearsoldgirl');
 
